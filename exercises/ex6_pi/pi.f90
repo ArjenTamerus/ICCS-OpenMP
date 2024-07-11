34fab5e4683f1805de8a8b247435ae4d4adfbc44
Arjen Tamerus <at748@cam.ac.uk>
2024-07-11 10:40:30 +0100

    Cleaned up exercise 6

--- exercises/ex6_pi/pi.f90
+++ exercises/ex6_pi/pi.f90
@@ -1,13 +1,21 @@
 ! Exercise: parallelise calculating Pi on CPU and/or GPU
 ! 
 ! You're on your own for this one!
-! 
+!
+! Can you calculate Pi correctly, in parallel?
+! Why not give it a try on both CPU or GPU
+!
+! If you get stuck, a GPU-offloaded solution can be found
+! in the `solution` folder
+!
+! Try playing around with the number of steps and see how it
+! affect execution time and accuracy
 program pi
   use omp_lib
 
   implicit none
 
-  integer, parameter :: nsteps = 1000000000
+  integer, parameter :: nsteps = 10000000
   integer, parameter :: dp = selected_real_kind(15,300)
   real(kind=dp), parameter :: pi_ref = 4.0_dp*DATAN(1.0_dp)
 
@@ -23,17 +31,19 @@ program pi
 
   time = omp_get_wtime()
 
-  !$omp target teams distribute parallel do simd shared(x) reduction(+:sum)
+!  !$omp target
   do i=1,nsteps
     x = (i-0.5) * step
     sum = sum + 4.0 / (1.0 + x * x)
   end do
-  !$omp end target teams distribute parallel do simd
+!  !$omp end target
 
   time = omp_get_wtime() - time
 
   pi_calc = step * sum
 
-  print *, pi_ref, pi_calc, time
+  print *, "Reference value of pi:", pi_ref
+  print *, "Calculated value of pi:", pi_calc
+  print *, "Seconds spent in computational loop:", time
 
 end program pi
--- exercises/ex6_pi/solutions/pi.f90
+++ exercises/ex6_pi/solutions/pi.f90
@@ -1,13 +1,21 @@
 ! Exercise: parallelise calculating Pi on CPU and/or GPU
 ! 
 ! You're on your own for this one!
-! 
+!
+! Can you calculate Pi correctly, in parallel?
+! Why not give it a try on both CPU or GPU
+!
+! If you get stuck, a GPU-offloaded solution can be found
+! in the `solution` folder
+!
+! Try playing around with the number of steps and see how it
+! affect execution time and accuracy
 program pi
   use omp_lib
 
   implicit none
 
-  integer, parameter :: nsteps = 1000000000
+  integer, parameter :: nsteps = 10000000
   integer, parameter :: dp = selected_real_kind(15,300)
   real(kind=dp), parameter :: pi_ref = 4.0_dp*DATAN(1.0_dp)
 
@@ -34,6 +42,8 @@ program pi
 
   pi_calc = step * sum
 
-  print *, pi_ref, pi_calc, time
+  print *, "Reference value of pi:", pi_ref
+  print *, "Calculated value of pi:", pi_calc
+  print *, "Seconds spent in computational loop:", time
 
 end program pi

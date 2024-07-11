36d706905e90880d3a9b2bc046eaa60774bd0c0c
Arjen Tamerus <at748@cam.ac.uk>
2024-07-11 10:57:59 +0100

    Exercise 1: remove default(none)

--- exercises/ex1_hello_world/hello_world.f90
+++ exercises/ex1_hello_world/hello_world.f90
@@ -14,24 +14,24 @@ program hello_world
   ! Exercise: Uncomment the OpenMP directives, implement the data-sharing clauses and re-run the code.
   ! What is the output? What is the expected output?
   ! Can you explain what happens?
-! !$omp parallel default(none)
+  !$omp parallel
   thread_count = omp_get_num_threads()
   my_thread = omp_get_thread_num()
 
   print *, "Hello from thread ", my_thread, " out of ", thread_count
-! !$omp end parallel
+  !$omp end parallel
 
   ! Exercise: Uncomment the OpenMP directives, implement the data-sharing clauses and re-run the code.
   ! What is the output? What is the expected output?
   ! Can you explain what happens?
   ! Do you need to add any extra clauses compared to the previous exercise?
   sum = 0
-! !$omp parallel do default(none)
+  !$omp parallel do
   do i=1,1024
     my_array(i) = i
     sum = sum + i
   end do
-! !$omp end parallel do
+  !$omp end parallel do
 
   print *, "first: ", my_array(1)
   print *, "last: ", my_array(1024)
